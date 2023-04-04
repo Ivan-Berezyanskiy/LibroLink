f699684406dab479b170b10c52054d9b8d8aa335
Іван Березянський <mamanadarin@gmail.com>
2023-04-04 19:13:42 +0300

    create model book

--- book/models.py
+++ book/models.py
@@ -1,3 +1,22 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
-# Create your models here.
+
+class Book(models.Model):
+    title = models.CharField(max_length=255)
+    author = models.ForeignKey(
+        get_user_model(),
+        on_delete=models.CASCADE,
+    )
+
+    class CoverChoice(models.TextChoices):
+        SOFT = "Soft"
+        HARD = "Hard"
+
+    cover = models.CharField(
+        max_length=4,
+        choices=CoverChoice.choices,
+        default=CoverChoice.HARD,
+    )
+    inventory = models.PositiveIntegerField()
+    daily_fee = models
