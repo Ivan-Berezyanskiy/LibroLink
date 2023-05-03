b11370a3dfe2bd5604e764ce62eed4bca9a6a0c7
Іван Березянський <mamanadarin@gmail.com>
2023-05-03 18:32:59 +0300

    Add save method in borrowing models.py

--- borrowing/models.py
+++ borrowing/models.py
@@ -1,5 +1,6 @@
 from django.contrib.auth import get_user_model
 from django.db import models
+from rest_framework.exceptions import ValidationError
 
 from book.models import Book
 
@@ -18,3 +19,12 @@ class Borrowing(models.Model):
         on_delete=models.CASCADE,
         related_name="borrowing",
     )
+
+    def save(self, *args, **kwargs) -> None:
+        if self.book.inventory < 1:
+            raise ValidationError(
+                "At the moment, we aren`t having this book."
+            )
+        self.book.inventory -= 1
+        self.book.save()
+        super(Borrowing, self).save(*args, **kwargs)
